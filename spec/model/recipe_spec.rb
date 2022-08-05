6b8c793c0d0a477dbf88d18ed3b0427930f4c53c
aboongm <aboongm@yahoo.com>
2022-08-05 15:44:50 +0530

    Corrections for linter

--- spec/model/recipe_spec.rb
+++ spec/model/recipe_spec.rb
@@ -1,21 +1,20 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-    before(:all) do
-        @user = User.create(name: "John Doe", email: "john@test.com")
+  before(:all) do
+    @user = User.create(name: 'John Doe', email: 'john@test.com')
+  end
+
+  describe 'validations' do
+    it 'is valid with a user' do
+      recipe = Recipe.new(user: @user)
+      expect(recipe).to be_valid
     end
+  end
 
-    describe "validations" do
-        it "is valid with a user" do
-            recipe = Recipe.new(user: @user)
-            expect(recipe).to be_valid
-        end
+  describe 'associations' do
+    it 'has many recipe foods' do
+      expect(Recipe.new.recipe_foods).to be_a_kind_of(ActiveRecord::Relation)
     end
-
-    describe "associations" do
-        it "has many recipe foods" do
-            expect(Recipe.new.recipe_foods).to be_a_kind_of(ActiveRecord::Relation)
-        end
-    end   
-
+  end
 end
